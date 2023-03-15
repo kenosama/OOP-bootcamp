64e192bd801c8c6ee241ee83e544c692f6da27b3
kenosama <pc.tcanomorant@me.com>
2023-03-15 16:20:18 +0100

    🚀 feat(articles.php): add isBreakingNews property to Article class constructor The Article class constructor now accepts a boolean parameter isBreakingNews which is set to false by default. This allows for the creation of articles that can be marked as breaking news.
    🐛 fix(index.php): fix missing argument in Article constructor
    ✨ feat(index.php): add missing argument to Article constructor
    The first change fixes a bug where the third argument was missing in the Article constructor. The second change adds the missing argument to the Article constructor, which improves the consistency of the code and ensures that the code runs without errors.

--- Case3/articles.php
+++ Case3/articles.php
@@ -3,12 +3,14 @@ class Article{
     private string $title;
     private string $text;
     private string $type;
+    private bool $isBreakingNews=false; 
 
-    public function __construct(string $title, string $text, string $type)
+    public function __construct(string $title, string $text, string $type,bool $isBreakingNews)
     {
         $this->title = $title;
         $this->text = $text;
         $this->type = $type;
+        $this->isBreakingNews=$isBreakingNews;
     }
     public function getTitle(): string
     {
--- Case3/index.php
+++ Case3/index.php
@@ -18,7 +18,7 @@ $articles=[
 
     But wait, there's more! Fennel also contains compounds that are known to stimulate the production of estrogen, the hormone responsible for giving women their feminine curves. So if you're looking for a natural way to enhance your assets, look no further than fennel.<br><br>
 
-    And there you have it, folks, fennel: the vegetable that makes you look good and smell good. Don't forget to stock up on this versatile veggie, your taste buds and your body will thank you.","Article"),
+    And there you have it, folks, fennel: the vegetable that makes you look good and smell good. Don't forget to stock up on this versatile veggie, your taste buds and your body will thank you.","Article",true),
     new Article("The Ultimate Scam Artists", "Dogs, man's best friend, or so they say. But have you ever stopped to think about what these furry little freeloaders are really up to? Let's take a closer look.<br><br>
 
 First of all, dogs are expert moochers. They'll stare you down with those big puppy eyes until you cave and give them a scrap of food from your plate. And don't even think about leaving your sandwich unattended, because it'll be gone in a flash.<br><br>
@@ -27,7 +27,7 @@ But that's not all, folks. Dogs are also masters of the guilt trip. If you dare 
 
 And let's not forget about the ultimate scam: doggy daycare. Yes, that's right, people are actually paying money to have their dogs hang out with other dogs all day. It's a genius business model, really. Dogs get to socialize and play all day, while their owners fork over the cash.<br><br>
 
-So there you have it, folks. Dogs may be cute and cuddly, but don't let their innocent faces fool you. They're just a bunch of scam artists looking for their next free meal.","Article"),
+So there you have it, folks. Dogs may be cute and cuddly, but don't let their innocent faces fool you. They're just a bunch of scam artists looking for their next free meal.","Article",false),
 
 new Commercial("Sausage: The Meaty Delight You Can't Resist", "Sausage: Because vegetables are what food eats.","Commercial"),
 
