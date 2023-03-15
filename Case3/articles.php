a5ee809ce0f86ef91e5212f4f089638c2814371d
kenosama <pc.tcanomorant@me.com>
2023-03-15 15:31:37 +0100

    🎨 style(articles.php): rename Articles class to Article This commit renames the Articles class to Article to improve naming consistency and to better reflect the class's purpose.
    🐛 fix(index.php): fix typo in Article class instantiation
    ✨ feat(index.php): add new article to articles array
    Fixed a typo in the instantiation of the Article class, which was causing an error. Also, added a new article to the articles array, expanding the content of the page.
    
    🚀 chore(index.php): add new objects to array
    Added a new Commercial object and a new Vacancies object to the array of objects. This improves the content of the page by adding more variety and information.

--- Case3/articles.php
+++ Case3/articles.php
@@ -1,5 +1,5 @@
 <?php
-class Articles{
+class Article{
     private string $title;
     private string $text;
     private string $type;
--- Case3/index.php
+++ Case3/index.php
@@ -11,15 +11,17 @@ require('ads.php');
 require('articles.php');
 require('vacancies.php');
 
+
+
 $articles=[
-    new Articles("Fennel: The Vegetable that Makes You Look Good and Smell Good", "Fennel is the ultimate multi-tasking vegetable. Not only does it taste great in salads, roasted, or sautéed, but it also has some unique properties that make it a true beauty booster.<br><br>
+    new Article("Fennel: The Vegetable that Makes You Look Good and Smell Good", "Fennel is the ultimate multi-tasking vegetable. Not only does it taste great in salads, roasted, or sautéed, but it also has some unique properties that make it a true beauty booster.<br><br>
 
     Did you know that fennel has been used as a breath freshener for centuries? Yes, forget about those artificial mints or gums, fennel seeds are the way to go. Chew on a few seeds after a meal, and you'll have the freshest breath in town.<br><br>
 
     But wait, there's more! Fennel also contains compounds that are known to stimulate the production of estrogen, the hormone responsible for giving women their feminine curves. So if you're looking for a natural way to enhance your assets, look no further than fennel.<br><br>
 
     And there you have it, folks, fennel: the vegetable that makes you look good and smell good. Don't forget to stock up on this versatile veggie, your taste buds and your body will thank you.","Article"),
-    new Articles("The Ultimate Scam Artists", "Dogs, man's best friend, or so they say. But have you ever stopped to think about what these furry little freeloaders are really up to? Let's take a closer look.<br><br>
+    new Article("The Ultimate Scam Artists", "Dogs, man's best friend, or so they say. But have you ever stopped to think about what these furry little freeloaders are really up to? Let's take a closer look.<br><br>
 
 First of all, dogs are expert moochers. They'll stare you down with those big puppy eyes until you cave and give them a scrap of food from your plate. And don't even think about leaving your sandwich unattended, because it'll be gone in a flash.<br><br>
 
@@ -27,11 +29,15 @@ But that's not all, folks. Dogs are also masters of the guilt trip. If you dare 
 
 And let's not forget about the ultimate scam: doggy daycare. Yes, that's right, people are actually paying money to have their dogs hang out with other dogs all day. It's a genius business model, really. Dogs get to socialize and play all day, while their owners fork over the cash.<br><br>
 
-So there you have it, folks. Dogs may be cute and cuddly, but don't let their innocent faces fool you. They're just a bunch of scam artists looking for their next free meal.","Article")
+So there you have it, folks. Dogs may be cute and cuddly, but don't let their innocent faces fool you. They're just a bunch of scam artists looking for their next free meal.","Article"),
+
+new Commercial("Sausage: The Meaty Delight You Can't Resist", "Sausage: Because vegetables are what food eats.","Commercial"),
+
+new Vacancies("Fullstack Developer", "Looking for a fullstack developer who can do everything from coding to fixing the printer. Must have superhuman abilities.","Job")
 ];
 
 
-$commercial=
-    new Commercial("Sausage: The Meaty Delight You Can't Resist", "Sausage: Because vegetables are what food eats.","Commercial");
 
-$vacancy= new Vacancies("Fullstack Developer", "Looking for a fullstack developer who can do everything from coding to fixing the printer. Must have superhuman abilities.","Job");
+
+
+
