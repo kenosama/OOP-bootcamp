ad4434480e24393a84259f5d8fbe1ab7db18aeb2
kenosama <pc.tcanomorant@me.com>
2023-03-16 10:04:32 +0100

    🎨 style(group.php): refactor Group class to StudentGroup and add Student class The Group class has been renamed to StudentGroup to better reflect its purpose. A new Student class has been added to represent individual students. This improves the readability and maintainability of the code by separating concerns and improving naming conventions.
    🚀 feat(index.php): add student groups and move students between groups
    This commit adds two groups of students, MCU and DCU, and calculates the average grade of each group. It then moves the top student from the MCU group to the DCU group and the lowest scoring student from the DCU group to the MCU group. The updated groups are then displayed, and the average grade of each group is calculated and displayed again.

--- Case4/group.php
+++ Case4/group.php
@@ -1,24 +1,54 @@
 <?php
-class Group{
-    private string $studentName;
-    private string $group;
-    private float $grade;
-    
+// Define a class to represent a group of students
+class StudentGroup
+{
+    // Property to store an array of students in the group
+    public $students;
 
-    public function __construct (string $studentName, float $grade, string $group){
-        $this->studentName=$studentName;
-        $this->grade=$grade;
-        $this->group=$group;
-    }
-    public function getStudentName():string{
-        return $this->studentName;
-    }
-    public function getGrade(): float
+    // Constructor to initialize the array of students
+    function __construct($studentNames, $studentGrades)
     {
-        return $this->grade;
+        $this->students = array();
+
+        // Create a new Student object for each name and grade, and add it to the array
+        for ($i = 0; $i < count($studentNames); $i++) {
+            $student = new Student($studentNames[$i], $studentGrades[$i]);
+            array_push($this->students, $student);
+        }
     }
-    public function getGroup(): string
+
+    // Function to calculate the average grade of the group
+    function calculateAverageGrade()
     {
-        return $this->group;
+        $total = 0;
+
+        // Add up the grades of all the students in the group
+        foreach ($this->students as $student) {
+            $total += $student->grade;
+        }
+
+        // Divide the total by the number of students to get the average grade
+        return $total / count($this->students);
     }
+
+    // Function to move a student from this group to another group
+    function moveStudentToGroup($studentName, $newGroup)
+    {
+        // Find the student in this group
+        $index = -1;
+        for ($i = 0; $i < count($this->students); $i++) {
+            if ($this->students[$i]->name == $studentName) {
+                $index = $i;
+                break;
+            }
+        }
+
+        // If the student was found, remove it from this group and add it to the new group
+        if ($index >= 0) {
+            $student = $this->students[$index];
+            array_splice($this->students, $index, 1);
+            array_push($newGroup->students, $student);
+        }
+    }
+
 }
--- Case4/index.php
+++ Case4/index.php
@@ -10,3 +10,59 @@ displayErrors();
 
 require('student.php');
 require('group.php');
+
+
+// Create two groups of students
+$mcuStudents = new StudentGroup(
+    array("Iron Man", "Captain America", "Thor", "Hulk", "Black Widow", "Hawkeye", "Spider-Man", "Black Panther", "Doctor Strange", "Ant-Man"),
+    array(1, 2, 3,4,5,6,7,8,9,10)
+);
+$dcuStudents = new StudentGroup(
+    array("Batman", "Superman", "Wonder Woman", "Flash", "Aquaman", "Green Lantern", "Cyborg", "Shazam", "Martian Manhunter", "Black Canary"),
+    array(1,2,3,4,5,6,7,8,9,10)
+);
+echo "<h1>display base groups</h1>";
+echo"<pre>";
+var_dump($mcuStudents);
+var_dump($dcuStudents);
+echo "</pre>";
+
+// Calculate and display the average grades of both groups
+echo "MCU Average Grade: " . $mcuStudents->calculateAverageGrade() . "<br>";
+echo "DCU Average Grade: " . $dcuStudents->calculateAverageGrade() . "<br><br>";
+
+// Move the top student from the MCU group to the DCU group, and the lowest scoring student from the DCU group to
+
+// Find the top student in the MCU group
+$topStudent = null;
+foreach ($mcuStudents->students as $student) {
+    if ($topStudent == null || $student->grade > $topStudent->grade) {
+        $topStudent = $student;
+    }
+}
+
+// Find the lowest scoring student in the DCU group
+$lowestScoringStudent = null;
+foreach ($dcuStudents->students as $student) {
+    if ($lowestScoringStudent == null || $student->grade < $lowestScoringStudent->grade) {
+        $lowestScoringStudent = $student;
+    }
+}
+echo"<h1>display the students with higher & lower points</h1>";
+echo "<pre>";
+var_dump($topStudent);
+var_dump($lowestScoringStudent);
+echo "</pre>";
+
+// Move the top student from the MCU group to the DCU group, and the lowest scoring student from the DCU group to the MCU group
+$mcuStudents->moveStudentToGroup($topStudent->name, $dcuStudents);
+$dcuStudents->moveStudentToGroup($lowestScoringStudent->name, $mcuStudents);
+echo "<h1>display the exchanged group</h1>";
+echo "<pre>";
+var_dump($mcuStudents);
+var_dump($dcuStudents);
+echo "</pre>";
+
+// Calculate and display the average grades of both groups again
+echo "MCU Average Grade: " . $mcuStudents->calculateAverageGrade() . "<br>";
+echo "DCU Average Grade: " . $dcuStudents->calculateAverageGrade() . "<br>";
