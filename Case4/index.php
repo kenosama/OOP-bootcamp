fdf926109a1e490d5182cb93e1ce22fde1d46d52
kenosama <pc.tcanomorant@me.com>
2023-03-16 10:16:44 +0100

    🎨 style(index.php): rename variables to improve readability The variable names for the two groups of students have been changed to theGoodOnes and theBadOnes to improve readability and make the code more understandable. The output of the var_dump function has also been updated to reflect the new variable names.
    🔀 merge(index.php): exchange top and lowest scoring students between groups
    The top student from the MCU group is moved to the DCU group, and the lowest scoring student from the DCU group is moved to the MCU group. The groups are now referred to as "theGoodOnes" and "theBadOnes" respectively. The average grades of both groups are recalculated and displayed.

--- Case4/index.php
+++ Case4/index.php
@@ -13,29 +13,29 @@ require('group.php');
 
 
 // Create two groups of students
-$mcuStudents = new StudentGroup(
-    array("Iron Man", "Captain America", "Thor", "Hulk", "Black Widow", "Hawkeye", "Spider-Man", "Black Panther", "Doctor Strange", "Ant-Man"),
+$theGoodOnes = new StudentGroup(
+    array("Missandei", "Tormund Giantsbane", "Ser Jorah Mormont", "Davos Seaworth", "Jacqen", "Brienne of Tarth", "Arya Stark", "Tyrion Lannister", "Daenerys Targaryen", "Jon Snow"),
     array(1, 2, 3,4,5,6,7,8,9,10)
 );
-$dcuStudents = new StudentGroup(
-    array("Batman", "Superman", "Wonder Woman", "Flash", "Aquaman", "Green Lantern", "Cyborg", "Shazam", "Martian Manhunter", "Black Canary"),
+$theBadOnes = new StudentGroup(
+    array("Petyr Baelish (Littlefinger)", "Melisandre", "Euron Greyjoy", "Walder Frey", "Viserys Targaryen", "Gregor Clegane (The Mountain)", "The Night King", "Ramsay Bolton", "Joffrey Baratheon", "Cersei Lannister"),
     array(1,2,3,4,5,6,7,8,9,10)
 );
 echo "<h1>display base groups</h1>";
 echo"<pre>";
-var_dump($mcuStudents);
-var_dump($dcuStudents);
+var_dump($theGoodOnes);
+var_dump($theBadOnes);
 echo "</pre>";
 
 // Calculate and display the average grades of both groups
-echo "MCU Average Grade: " . $mcuStudents->calculateAverageGrade() . "<br>";
-echo "DCU Average Grade: " . $dcuStudents->calculateAverageGrade() . "<br><br>";
+echo "MCU Average Grade: " . $theGoodOnes->calculateAverageGrade() . "<br>";
+echo "DCU Average Grade: " . $theBadOnes->calculateAverageGrade() . "<br><br>";
 
 // Move the top student from the MCU group to the DCU group, and the lowest scoring student from the DCU group to
 
 // Find the top student in the MCU group
 $topStudent = null;
-foreach ($mcuStudents->students as $student) {
+foreach ($theGoodOnes->students as $student) {
     if ($topStudent == null || $student->grade > $topStudent->grade) {
         $topStudent = $student;
     }
@@ -43,7 +43,7 @@ foreach ($mcuStudents->students as $student) {
 
 // Find the lowest scoring student in the DCU group
 $lowestScoringStudent = null;
-foreach ($dcuStudents->students as $student) {
+foreach ($theBadOnes->students as $student) {
     if ($lowestScoringStudent == null || $student->grade < $lowestScoringStudent->grade) {
         $lowestScoringStudent = $student;
     }
@@ -55,14 +55,14 @@ var_dump($lowestScoringStudent);
 echo "</pre>";
 
 // Move the top student from the MCU group to the DCU group, and the lowest scoring student from the DCU group to the MCU group
-$mcuStudents->moveStudentToGroup($topStudent->name, $dcuStudents);
-$dcuStudents->moveStudentToGroup($lowestScoringStudent->name, $mcuStudents);
+$theGoodOnes->moveStudentToGroup($topStudent->name, $theBadOnes);
+$theBadOnes->moveStudentToGroup($lowestScoringStudent->name, $theGoodOnes);
 echo "<h1>display the exchanged group</h1>";
 echo "<pre>";
-var_dump($mcuStudents);
-var_dump($dcuStudents);
+var_dump($theGoodOnes);
+var_dump($theBadOnes);
 echo "</pre>";
 
 // Calculate and display the average grades of both groups again
-echo "MCU Average Grade: " . $mcuStudents->calculateAverageGrade() . "<br>";
-echo "DCU Average Grade: " . $dcuStudents->calculateAverageGrade() . "<br>";
+echo "MCU Average Grade: " . $theGoodOnes->calculateAverageGrade() . "<br>";
+echo "DCU Average Grade: " . $theBadOnes->calculateAverageGrade() . "<br>";
